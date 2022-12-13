7216067f8881778d9be25a93ede185addec715a1
xavixava <zerezeze@gmail.com>
2022-12-13 13:04:50 +0000

    started saving player info

--- src/chase.h
+++ src/chase.h
@@ -1,8 +1,9 @@
 #define WINDOW_SIZE 20
 #define SOCK_ADDRESS "/tmp/server_sock"
 
+// if server sends disconecting then it means it cannot accept more clients
 typedef struct ball_message{	//later on define field status messages
-    int type; // 0-conection, 1-movement, 3-refusing char 
+    int type; // 0-conection, 1-movement, 2-refusing char 
     char arg;  // conection: c-conecting, d-disconecting; movement: u, d, l, r
     char c; //warn which char
 } ball_message;
--- src/server.c
+++ src/server.c
@@ -7,6 +7,8 @@
 
 #include "chase.h"
 
+#define MAX_PLAYERS 10
+
 WINDOW * message_win;
 
 typedef enum direction_t {UP, DOWN, LEFT, RIGHT} direction_t;
@@ -14,6 +16,7 @@ typedef enum direction_t {UP, DOWN, LEFT, RIGHT} direction_t;
 typedef struct player_position_t{
     int x, y;
     char c;
+    unsigned int health_bar;
 } player_position_t;
 
 void new_player (player_position_t * player, char c){
@@ -87,13 +90,23 @@ int create_socket()
 	return sock_fd;
 }
 
+int search_player(player_position_t vector[], char c)
+{
+	int i;
+	for(i=0; i<MAX_PLAYERS; i++)if(vector[i]==c) return i;
+	return -1;
+}
+
 int main(){
 
-	int fd, n;
+	int fd, i, n, player_count=0;
 	struct sockaddr_un client_addr;
         socklen_t client_addr_size = sizeof(struct sockaddr_un);
 	ball_message m;
+	player_position_t players[10];
 
+	for(i=0; i<MAX_PLAYERS; i++) players.c = '\0';
+	
 	fd = create_socket();
 
 	initscr();		    	/* Start curses mode 		*/
@@ -107,16 +120,42 @@ int main(){
 	wrefresh(my_win);
     
 	/* creates a window and draws a border */
-	message_win = newwin(5, WINDOW_SIZE, WINDOW_SIZE, 0);
+	message_win = newwin(15, WINDOW_SIZE, WINDOW_SIZE, 0);
 	box(message_win, 0 , 0);	
 	wrefresh(message_win);
 	
     	while(1){
 	
 		n = recvfrom(fd, &m, sizeof(ball_message), 0, ( struct sockaddr *)&client_addr, &client_addr_size);
-		if(n == -1)perror("Send error(please press ctrl+C)");
+		if(n == -1)perror("recv error(please press ctrl+C)");
 			
 		mvwprintw(message_win, 1,1,"Received: %c %c %c", m.type, m.arg, m.c);
+
+		switch (m.type)
+		{
+			case '0':
+				if(m.arg == 'c')
+				{
+					if(search_player(players, m.c)==-1)
+					{	
+						i=0;
+						while(players[i]!='\0' && i<MAX_PLAYERS) i++;
+						players[i].x = WINDOW_SIZE/2;
+						players[i].y = WINDOW_SIZE/2;
+						players[i].c = m.c;
+						players[i].health_bar = 10;
+					}
+					else
+					{
+						//repeated character
+					}	
+				}
+				else if (m.arg == 'd')
+				{
+					//disconect client
+				}
+				else mvwprintw(message_win, 2,1,"Message poorly formatted.", m.type, m.arg, m.c);
+		}
 		wrefresh(message_win);
 	}
 
