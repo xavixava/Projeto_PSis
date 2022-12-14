519423c75922b88971117a995ca79ee6b3fe06d1
xavixava <zerezeze@gmail.com>
2022-12-14 18:43:21 +0000

    added ball info response by server

--- src/chase.h
+++ src/chase.h
@@ -1,9 +1,17 @@
 #define WINDOW_SIZE 20
 #define SOCK_ADDRESS "/tmp/server_sock"
+#define PRIZE_ADDR i "/tmp/prize_sock"
 
 // if server sends disconecting then it means it cannot accept more clients
-typedef struct ball_message{	//later on define field status messages
-    int type; // 0-conection, 1-movement, 2-refusing char 
+typedef struct client_message{	//later on define field status messages
+    int type; // 0-conection, 1-movement 
     char arg;  // conection: c-conecting, d-disconecting; movement: u, d, l, r
     char c; //warn which char
-} ball_message;
+} client_message;
+
+typedef struct server_message{	//later on define field status messages
+    int type; // 0-conection accepted, 1-no more characters, 2-refusing char, 3-movement response
+    int x, y; // x, y positions
+    int health; // character health
+    int elements; // number of elements to be able to receive field status message
+} server_message;
